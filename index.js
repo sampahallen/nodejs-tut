a9f5bb1e785cfc95010b1e50dd2a0e13a9714c5a
sampahallen <sampahallen@gmail.com>
2025-09-27 08:59:20 +0000

    refactor: improve code readability and error handling in user routes

--- index.js
+++ index.js
@@ -22,16 +22,18 @@ app.post('/api/users', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-  res.send(users.filter(u => u.isDeleted === false));
+  res.send(users.filter((u) => u.isDeleted === false));
 });
 
 app.get('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((u) => u.id === userId && u.isDeleted === false);
+
+  if (!user) {
+    res.status(400).send({ error: 'User not Found' });
+  }
+
   try {
-    const userId = parseInt(req.params.id);
-    const user = users.find((u) => u.id === userId && u.isDeleted === false);
-    if (!user) {
-      res.status(400).send({ error: 'User not Found' });
-    }
     res.status(200).send(user);
   } catch (error) {
     res.status(500).send({ error: 'Server error' });
@@ -41,7 +43,7 @@ app.get('/api/users/:id', (req, res) => {
 app.put('/api/users/:id', (req, res) => {
   try {
     const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId && u.isDeleted === false);
+    const user = users.find((u) => u.id === userId && u.isDeleted === false);
     user.name = req.body.name ? req.body.name : user.name;
     user.age = req.body.age ? req.body.age : user.age;
     user.city = req.body.city ? req.body.city : user.city;
@@ -57,19 +59,18 @@ app.put('/api/users/:id', (req, res) => {
 });
 
 app.delete('/api/users/:id', (req, res) => {
-    try{
-        const user = users.find(u => u.id === parseInt(req.params.id));
-        if (!user) {
-            res.status(400).send({"error": "User not found"})
-        }
-        user.isDeleted = true;
-
-        res.status(200).send({"message": "User has been deleted"})
-
-    } catch (error) {
-        res.status(500).send({"error": "Server error"})
+  try {
+    const user = users.find((u) => u.id === parseInt(req.params.id));
+    if (!user) {
+      res.status(400).send({ error: 'User not found' });
     }
-})
+    user.isDeleted = true;
+
+    res.status(200).send({ message: 'User has been deleted' });
+  } catch (error) {
+    res.status(500).send({ error: 'Server error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
